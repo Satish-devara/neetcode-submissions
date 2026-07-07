b2757e631f051b1dd8f51e2024979a0aebf17c2b
Sai Satish devara <sd1761256@gmail.com>
2026-07-07 14:14:38 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int ans = 0;
+        for(int i = 0;  i < prices.length; i++){
+            min = Math.min(min, prices[i]);
+            if(prices[i] > min){
+                ans = Math.max(ans, prices[i] - min);
+            }
+        }
+
+        return ans;
+    }
+}
